4429f4f8b68bd2df31cb1eb71b583f35af387680
Abaan5467 <97291077+Abaan5467@users.noreply.github.com>
2022-02-24 17:47:03 +0530

    Add files via upload

---
+++ WhatsApp.py
@@ -0,0 +1,11 @@
+from operator import index
+from pywhatkit.whats import sendwhatmsg
+
+number = input("Enter the number with with the calling code: ")
+time=input("Enter the time at which you want to send the message in 24 hour format: ")
+message=input("Enter the message: ")
+x=time.split(':')
+hours=int(x[0])
+mins=int(x[1])
+sendwhatmsg(number,message,hours,mins)
+print("Message sent")
