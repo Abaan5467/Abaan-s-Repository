5f0f51ce1252f718fa826f1ff385ca1245fddd89
Abaan5467 <97291077+Abaan5467@users.noreply.github.com>
2022-03-01 15:59:30 +0530

    Update Data Plotter.py

--- Data Plotter.py
+++ Data Plotter.py
@@ -1,15 +1,38 @@
-import matplotlib.pyplot as plt
-import pandas
-import seaborn
+from cProfile import label
+from tkinter import *
+from pandas import read_csv
+from seaborn import countplot
+from matplotlib.pyplot import show
 
-path=input("Enter the path of the csv file with two backslashes: ")
-column=input("Enter the column name to be ploted: ")
-h = input("Enter the hue or n to skip this: ")
-df=pandas.read_csv(path)
+frame = Tk()
+frame.geometry('800x500')
+frame.title('Data Plotter')
 
-if h=='n':
-    seaborn.countplot(x=column,data=df)
-    plt.show()
-else:
-    seaborn.countplot(x=column,hue=h,data=df)
-    plt.show()
+def data():
+    df = read_csv(path_box.get())
+    if hue_box.get() == 's':
+        countplot(x=column_box.get(),data=df)
+        show()
+    else:
+        countplot(x=column_box.get(),hue =hue_box.get(),data=df)
+        show()
+
+path = Label(frame,text='Enter the path of the csv file with two backslashes: ')
+column = Label(frame,text='Enter the name of the colmn to be plotted: ')
+hue = Label(frame,text='Enter the hue or s to skip this: ')
+btn = Button(frame,text='Plot',padx=15,pady=3,fg="red",bg='orange',command=data)
+
+path_box=Entry(frame,width=50)
+column_box=Entry(frame,width=50)
+hue_box=Entry(frame,width=50)
+
+path.grid(row=0,column=0)
+column.grid(row=1,column=0)
+hue.grid(row=2,column=0)
+btn.grid(row=3,column=0)
+
+path_box.grid(row=0,column=1)
+column_box.grid(row=1,column=1)
+hue_box.grid(row=2,column=1)
+
+frame.mainloop()
